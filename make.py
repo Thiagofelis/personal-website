d4f277b04e74b874949d9b691df0fa9a15459eed
Thiago Felicissimo <thiagofelis-br@hotmail.com>
2021-02-01 11:59:44 +0100

    code commented, directories reorganized

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 # files used:
 # --- configuration files ---
-# config.txt       -> sets the available languages and the page's name
-# subtitle.txt     -> small description in each language
+# config.txt       -> sets the available languages and my name
+# subtitles.txt     -> subtitles in each language
 # pages.txt        -> enumerates the pages and configures which pages are present in each language
 # paths.txt        -> paths for files from the home dir
 # --- page models ---
@@ -16,12 +16,14 @@ from os.path import exists
 from shutil import copyfile, rmtree
 
 
-# model for entries 
+# models for entries 
 flagentry = "<li class=\"flag\"><a href=!link!><img src=!flagpath! align=left height=20></a></li>"
 normalentry = "<li><a href=!link!>!pagename!</a></li>"
 activeentry = "<li><a href=!link!><u>!pagename!</u></a></li>"
 
 def readAndCut(path):
+# reads lines, remove empty liens and lines with coments,
+# and then splits lines at spaces
     s = open(path).read().splitlines()
     # removes empty line
     s = list(filter(lambda v: v != '', s))
@@ -30,27 +32,37 @@ def readAndCut(path):
     return [i.split() for i in s]
 
 def readNoCut(path):
+# reads lines, remove empty liens and lines with coments,
+# but does NOT split lines at spaces    
     s = open(path).read().splitlines()
     # removes empty line
     s = list(filter(lambda v: v != '', s))
     # filter comments
     return list(filter(lambda v: v[0] != '#', s))
 
-def putCitation(str):
+def putQuotes(str):
+# put " " around string, to be used for links and paths    
     return '\"' + str + '\"'
 
 def createsNavigationBar(i, j, languages, pagename, pagedisplayname, pageinlanguage):
+# creates navigation bar of the i-th page on the j-th language
+# order of languages and pages is the one given by pagename and languages
     bar = ""
+    # adds entries to other pages of the same language
     for k in range(len(pagename)):
         if(pageinlanguage[k][j] == '1'):
-            s = (normalentry if k != i else activeentry).replace('!pagename!', pagedisplayname[k][j]).replace('!link!', putCitation(pagename[k] + '.html'))
+            # if the k-th page is not available in the current language (j) then we dont add it to the navigation bar
+            s = (normalentry if k != i else activeentry).replace('!pagename!', pagedisplayname[k][j]).replace('!link!', putQuotes(pagename[k] + '.html'))
             bar = bar + s + '\n'
+    # adds entries to other languages of the same page
     for k in range(len(languages)):
-        s = flagentry.replace('!flagpath!', putCitation(("" if j == 0 else "../") + 'flags/' + languages[k] + ('_on' if k == j else '_off') + ".png"))
+        # flag of the current language takes the on version, while other languages take the off
+        s = flagentry.replace('!flagpath!', putQuotes(("" if j == 0 else "../") + 'flags/' + languages[k] + ('_on' if k == j else '_off') + ".png"))
+        # adds the link
         s = s.replace('!link!',
-                      putCitation(("" if j == 0 else "../") +
+                      putQuotes(("" if j == 0 else "../") +
                                   ("" if k == 0 else languages[k] + '/') +
-                                  # if the page is not available in the targeted languages, redirects to homepage
+                                  # if the page is not available in the targeted language, redirects to homepage of the target langage
                                   (pagename[i] if pageinlanguage[i][k] == '1' else 'index') +
                                   ".html"))
         bar = bar + s + '\n'
@@ -64,23 +76,32 @@ model  = open('model.html').read()
 pages = readAndCut('pages.txt')
 config = readNoCut('config.txt')
 paths = readAndCut('paths.txt')
-subtitle = readNoCut('subtitle.txt')
+subtitles = readNoCut('subtitles.txt')
 
+# first line of config contains the list of languages
 languages = config[0].split()
+# second line of config contains my name
+# doest not split because my name can have spaces
 myname = config[1]
+# column 0 of pages contains the names
 pagename = [pg[0] for pg in pages]
+# columns 1 to num_languages contains indicator if page is present in language
 pageinlanguage = [pg[1:(1 + len(languages))] for pg in pages]
+# columns (num_languages + 1) to (2 num_languages) contains de display name in each language
+# if page is not present in the corresponding language, then content will not be used
 pagedisplayname = [pg[(1 + len(languages)):(1 + 2 * len(languages))] for pg in pages]
-                  
+
+# reads the contents of the declared pages, on the order given
 pagecontents = []
 for name in pagename:
     pagecontents.append(
         open(name + '.html').read().split('!split!')
     )
 
+# reads homepage contents
 homecontents = open('home.html').read().split('!split!')    
 
-# creates out directory and subdirectories for the other languages
+# creates out directory, a subdirecoty for the flags and subdirectories for the other languages
 if exists('out'):
     rmtree('out')
 mkdir('out')
@@ -96,25 +117,8 @@ for lang in languages:
 for lang in languages[1:]:
     if not exists('out/' + lang):
         mkdir('out/' + lang)
-        
-# creates home page        
-#for i in range(len(languages)):
- #   path = 'out/index.html' if i == 0 else 'out/' + languages[i] + '/index.html'
-  #  s = index_model.replace('!name!', myname).replace('!textundername!', subtitle[i]).replace('!pagecontent!', homecontents[i])
-   # for pt in paths:
-#        s = s.replace(pt[0], putCitation(pt[1] if i == 0 else '../' + pt[1]))
- #   open(path, 'w').write(s)
 
-# creates other pages       
-#for j in range(len(pagename)):
- #   for i in range(len(languages)):
-  #      if pageinlanguage[j][i] == '1':
-   #         path = 'out/' + pagename[j] + '.html' if i == 0 else 'out/' + languages[i] + '/' + pagename[j] + '.html'
-    #        s = model.replace('!name!', myname).replace('!pagecontent!', pagecontents[j][i]).replace('!pagename!', pagedisplayname[j][i])
-     #       for pt in paths:
-      #          s = s.replace(pt[0], putCitation(pt[1] if i == 0 else '../' + pt[1]))
-       #     open(path, 'w').write(s)
-
+# adds homepage data to the lists        
 contents = [homecontents] + pagecontents
 pagename = ['index'] + pagename
 pagedisplayname = [['Home' for i in range(len(languages))]] + pagedisplayname
@@ -123,13 +127,18 @@ pageinlanguage = [['1' for i in range(len(languages))]] + pageinlanguage
             
 for j in range(len(pagename)):
     for i in range(len(languages)):
+        # if page not present in the language, dont create it
         if pageinlanguage[j][i] == '1':
+            # defines page path
             path = 'out/' + pagename[j] + '.html' if i == 0 else 'out/' + languages[i] + '/' + pagename[j] + '.html'
+            # replace model with all the specific data
             s = (index_model if j == 0 else model).replace('!name!', myname).replace('!pagecontent!', contents[j][i])
-            s = s.replace('!textundername!', subtitle[i]).replace('!pagename!', pagedisplayname[j][i])
+            s = s.replace('!textundername!', subtitles[i]).replace('!pagename!', pagedisplayname[j][i])
             s = s.replace('!navigationbar!',
                           createsNavigationBar(j,i,languages,pagename,pagedisplayname,pageinlanguage))
+            # replace paths in the page
             for pt in paths:
-                s = s.replace(pt[0], putCitation(pt[1] if i == 0 else '../' + pt[1]))
+                s = s.replace(pt[0], putQuotes(pt[1] if i == 0 else ('../' + pt[1])))
+            # write in file
             open(path, 'w').write(s)
             
